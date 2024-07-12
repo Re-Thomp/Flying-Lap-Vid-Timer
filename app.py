f98bd952cfd29c7c0914efd1c16411a3ff476f9d
Re-Thomp <robbiethompy@gmail.com>
2024-07-12 17:55:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -38,12 +38,12 @@ def main():
         adjusted_duration = duration - (1 / fps)
 
         # Select start and end points with frame preview
-        start_time = st.slider("Select start time", 0.0, adjusted_duration, 0.0, 0.01)
+        start_time = st.slider("Select start time", 0.0, adjusted_duration, 0.0, 0.001)
         start_frame = get_frame_at_time(video, start_time)
         if start_frame:
             st.image(start_frame, caption=f"Start Frame at {start_time:.2f} seconds", use_column_width=True)
 
-        end_time = st.slider("Select end time", 0.0, adjusted_duration, adjusted_duration, 0.01)
+        end_time = st.slider("Select end time", 0.0, adjusted_duration, adjusted_duration, 0.001)
         end_frame = get_frame_at_time(video, end_time)
         if end_frame:
             st.image(end_frame, caption=f"End Frame at {end_time:.2f} seconds", use_column_width=True)
