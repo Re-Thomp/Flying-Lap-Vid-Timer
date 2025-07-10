12cb3c57102daca635d82cc53947cd88f5bed9a6
Re-Thomp <robbiethompy@gmail.com>
2025-07-10 18:01:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Thank you Chat for the help
 import streamlit as st
-from moviepy.editor import VideoFileClip
+from moviepy.video import VideoFileClip
 import tempfile
 import os
 from PIL import Image
@@ -46,7 +46,7 @@ def main():
 
         # Load video and find stats
         video_path = temp_file.name
-        video = moviepy.editor.VideoFileClip(video_path)
+        video = VideoFileClip(video_path)
         fps = video.fps
         duration = video.duration
 
